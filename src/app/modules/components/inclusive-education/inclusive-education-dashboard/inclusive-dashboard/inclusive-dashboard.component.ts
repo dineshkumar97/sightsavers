64c2be83bd6d33fce38251b87cae19416dbad8d7
dineshkumar97 <dineshkumarppn07@gmail.com>
2024-01-29 22:52:09 +0530

    styles

--- src/app/modules/components/inclusive-education/inclusive-education-dashboard/inclusive-dashboard/inclusive-dashboard.component.ts
+++ src/app/modules/components/inclusive-education/inclusive-education-dashboard/inclusive-dashboard/inclusive-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+interface City {
+  name: string,
+  code: string
+}
 
 @Component({
   selector: 'app-inclusive-dashboard',
@@ -8,4 +12,16 @@ import { Component } from '@angular/core';
 export class InclusiveDashboardComponent {
   selectedCountryAdvanced: any[] = [];
   filteredCountries: any[] = [];
+  cities: City[];
+
+  selectedCity: City;
+  constructor() {
+    this.cities = [
+        {name: 'New York', code: 'NY'},
+        {name: 'Rome', code: 'RM'},
+        {name: 'London', code: 'LDN'},
+        {name: 'Istanbul', code: 'IST'},
+        {name: 'Paris', code: 'PRS'}
+    ];
+}
 }
